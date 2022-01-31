37939caa710000cf4611bab952901fd4495aa056
Jason.Magniza <magnizaj@outlook.com>
2022-01-30 19:04:05 -0600

    updated clock to change between 12 hour and 24 hour formats

--- Clock Web App/index.js
+++ Clock Web App/index.js
@@ -1,7 +1,20 @@
+let timeFormat = 0;
+let reformattedHours;
+let middayIndicator;
+
 function updateTime() {
     let currentTime = new Date();
-    let displayTime = `${leadingZeros(currentTime.getHours())} : ${leadingZeros(currentTime.getMinutes())} : ${leadingZeros(currentTime.getSeconds())}`;
-    document.getElementById("clock").textContent = displayTime;
+    console.log(currentTime.getTimezoneOffset())
+    if (timeFormat === 0) {
+        reformattedHours = currentTime.getHours();
+        middayIndicator = '';
+    }
+    else {
+        reformattedHours = (currentTime.getHours() > 12) ? currentTime.getHours() - 12 : currentTime.getHours();
+        middayIndicator = (currentTime.getHours() > 12) ? " PM" : " AM";
+    }
+    let displayTime = `${leadingZeros(reformattedHours)} : ${leadingZeros(currentTime.getMinutes())} : ${leadingZeros(currentTime.getSeconds())}`;
+    document.getElementById("clock").textContent = displayTime + middayIndicator;
 };
 
 function leadingZeros(num) {
@@ -9,5 +22,14 @@ function leadingZeros(num) {
     while (num.length < 2) num = "0" + num;
     return num;
 };
+
+document.getElementById("12-hour").addEventListener("click", function () {
+    timeFormat = 1;
+    updateTime();
+})
+document.getElementById("24-hour").addEventListener("click", function () {
+    timeFormat = 0;
+    updateTime();
+})
 
 setInterval(updateTime, 1000);
