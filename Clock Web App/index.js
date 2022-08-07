698cc1f0289cfa16e9ebe6b923ea5a3c5ea7ab70
JasonGrimmLaptop <debatewhale@gmail.com>
2022-08-07 17:15:56 -0500

    added time zone selection section

--- Clock Web App/index.js
+++ Clock Web App/index.js
@@ -1,21 +1,42 @@
 let timeFormat = 0;
 let reformattedHours;
 let midDayIndicator;
+let offsetID;
+let displayedTimeZone;
+let timeZones = {
+    tz1: 1,
+    tz2: 1,
+    tz3: 0,
+    tz4: -1,
+    tz5: -2,
+    tz6: -3,
+    tz7: -4,
+    tz8: -8
+}
 
 function updateTime() {
     let currentTime = new Date();
-    console.log(currentTime.getTimezoneOffset())
+    offsetTime();
+    let displayHours = currentTime.getHours() + timeZones[offsetID];
     if (timeFormat === 0) {
-        reformattedHours = currentTime.getHours();
+        reformattedHours = displayHours;
         midDayIndicator = '';
     }
     else {
-        reformattedHours = (currentTime.getHours() > 12) ? currentTime.getHours() - 12 : currentTime.getHours();
-        midDayIndicator = (currentTime.getHours() > 12) ? " PM" : " AM";
+        reformattedHours = (displayHours > 12) ? displayHours - 12 : displayHours;
+        midDayIndicator = (displayHours > 12) ? " PM" : " AM";
     }
     let displayTime = `${leadingZeros(reformattedHours)} : ${leadingZeros(currentTime.getMinutes())} : ${leadingZeros(currentTime.getSeconds())}`;
     document.getElementById("clock").textContent = displayTime + midDayIndicator;
 };
+
+function offsetTime() {
+    document.querySelectorAll('input.time-zone').forEach(option => {
+        if(option.checked) {
+            offsetID = option.id;
+        }
+    });
+}
 
 function leadingZeros(num) {
     num = num.toString();
@@ -27,6 +48,7 @@ document.getElementById("12-hour").addEventListener("click", function () {
     timeFormat = 1;
     updateTime();
 })
+
 document.getElementById("24-hour").addEventListener("click", function () {
     timeFormat = 0;
     updateTime();
